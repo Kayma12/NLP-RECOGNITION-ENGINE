680a349a3200b6c8aebd4e11fc23687346dcbda3
Filip Skrilec <filip.skrilec@fdmgroup.com>
2020-05-20 17:26:26 +0200

    Added read_in_file function for reading in .doc and .docx files

---
+++ read_in_doc_docx_files.py
@@ -0,0 +1,9 @@
+import textract
+
+# The following function takes a file and/or file path, either with doc or
+# docx extension, and returns the text representation of it, i.e. a txt file.
+
+def read_in_file(file):
+    text_file_repr = textract.process(file)
+    text_file_repr = text_file_repr.decode('utf-8')
+    return text_file_repr
