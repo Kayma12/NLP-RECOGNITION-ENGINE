56ea2d6d5e41561e74b53ddc2740a1ea4c88612b
kayan <kayan.marshall@gmail.com>
2020-06-04 12:58:30 +0100

    add service file

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from src.controller import blueprint as module_a
-from src import database
 
 app = Flask(__name__, template_folder='./templates')
 app.secret_key = "secretkey"
-database.add_consultant()
 
 # register Blueprint
 app.register_blueprint(module_a)
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from src import database
+
+database.add_consultant()
