6eea1f7d8dd0169130e8b94e359a2071ba566dd1
kayan <kayan.marshall@gmail.com>
2020-06-04 12:20:47 +0100

    add consultant

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from src.controller import blueprint as module_a
+from src import database
 
 app = Flask(__name__, template_folder='./templates')
 app.secret_key = "secretkey"
+database.add_consultant()
 
 # register Blueprint
 app.register_blueprint(module_a)
@@ -10,3 +12,7 @@ app.register_blueprint(module_a)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
--- src/database.py
+++ src/database.py
@@ -1,4 +1,24 @@
 from pymongo import MongoClient
 
 mdb_client = MongoClient(host="mongodb://localhost:27017", connect=False)
-mdb = mdb_client["Name of collection"]
+mdb = mdb_client["Consultants"]
+
+
+# Populate db
+def add_consultant():
+    consultant = mdb.consultant.insert_one({
+        "name": {
+            "first_name": "Susan",
+            "last_name": "Campbell"
+        },
+        "streams": {
+            "development": True,
+            "business_analysis": False,
+            "business_intelligence": False
+        },
+        "skills": {
+            "Java": 3,
+            "Python": 9,
+        }
+    })
+    print("user added successfully !!!")
