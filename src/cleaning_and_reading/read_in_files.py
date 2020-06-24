fdc343b8eaa9c2e4845e54d52612f9a0405e589c
Muhammad Salman <hactoor@hotmail.com>
2020-06-24 12:37:33 +0100

    'Tester'

--- src/cleaning_and_reading/read_in_files.py
+++ src/cleaning_and_reading/read_in_files.py
@@ -1,10 +1,12 @@
 import textract
-
+import sys
 
 # The following function takes a file and/or file path, either with doc or
 # docx extension, and returns the text representation of it, i.e. a txt file.
+
 
 def read_in_doc_docx_file(file):
     text_file_repr = textract.process(file)
     text_file_repr = text_file_repr.decode('utf-8')
     return text_file_repr
+
