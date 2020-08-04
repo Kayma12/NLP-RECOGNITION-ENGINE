6a3aca7da70d7df481f5a3b4bd7d129c0cf93ead
kayan <kayan.marshall@gmail.com>
2020-08-04 12:20:37 +0200

    pop up stopped

--- Scraping_job_descriptions.py
+++ Scraping_job_descriptions.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException, StaleElementReferenceException
 import time
 import pandas as pd
 
@@ -21,6 +21,11 @@ map_of_streams = {
     'Information Security Management': ['information security'], 'IT Service Management': ['technical support'],
     'PMO': ['project manager'], 'Robotic Process Automation': ['robotic automation'],
     'Testing': ['software tester', 'software test', 'java test engineer']}
+
+job_titles = []
+links = []
+streams = []
+descriptions = []
 
 
 def scrape_web_job_description(map_of_streams):
@@ -42,6 +47,7 @@ def scrape_web_job_description(map_of_streams):
     driver.get('https://www.indeed.co.uk/advanced_search')
 
     for stream, job_descriptions in map_of_streams.items():
+        print(job_descriptions, 'here >>>')
 
         for job_description in job_descriptions:
 
@@ -62,44 +68,47 @@ def scrape_web_job_description(map_of_streams):
             sort_option.click()
             search_button = driver.find_element_by_xpath('//*[@id="fj"]')
             search_button.click()
+            try:
+                close_cookie_popup = driver.find_element_by_id("onetrust-accept-btn-handler")
+                close_cookie_popup.click()
+            except:
+                print("No cookies p[opuop")
 
             # Save the data to a table
             # let the driver wait 3 seconds to locate the element before exiting out
             driver.implicitly_wait(3)
 
-            job_titles = []
-            links = []
-            Streams = []
-            descriptions = []
-
-            for i in range(0, 1):
+            for i in range(0, 2):
 
                 job_card = driver.find_elements_by_xpath('//div[contains(@class,"clickcard")]')
 
                 for job in job_card:
 
                     try:
-                        Streams.append(stream)
+
+                        # driver.implicitly_wait(3)
                         title = job.find_element_by_xpath('.//h2[@class="title"]//a').text
+                        job_titles.append(title)
+                        links.append(job.find_element_by_xpath('.//h2[@class="title"]//a').get_attribute(name="href"))
+                        streams.append(stream)
+
                     except:
                         title = job.find_element_by_xpath('.//h2[@class="title"]//a').get_attribute(name="title")
-                    job_titles.append(title)
-                    links.append(job.find_element_by_xpath('.//h2[@class="title"]//a').get_attribute(name="href"))
+                        job_titles.append(title)
+                        links.append(job.find_element_by_xpath('.//h2[@class="title"]//a').get_attribute(name="href"))
+                        streams.append(stream)
+
+                try:
+                    next_page = driver.find_element_by_xpath('//a[@aria-label={}]//span[@class="pn"]'.format(i + 2))
+                    next_page.click()
+
+                except ElementClickInterceptedException:
 
                     try:
-                        next_page = driver.find_element_by_xpath('//a[@aria-label={}]//span[@class="pn"]'.format(i + 2))
+                        time.sleep(8)  # Sometimes the pop-up takes time to load
+                        close_cookie_popup = driver.find_element_by_id("onetrust-accept-btn-handler")
+                        close_cookie_popup.click()
 
-                        next_page.click()
-
-
-
-
-                    except ElementClickInterceptedException:
-                        driver.findElement(By.xpath("//a[@class='button allow']/span[text()='Allow cookies']")).click();
-                        # If pop-up overlay appears, click the X button to close
-                        time.sleep(2)  # Sometimes the pop-up takes time to load
-                        close_popup = driver.find_element_by_id("popover-x")
-                        close_popup.click()
 
 
 
@@ -110,30 +119,30 @@ def scrape_web_job_description(map_of_streams):
                         next_page = driver.find_element_by_xpath('//a[@aria-label="Next"]//span[@class="np"]')
                         next_page.click()
 
-            # print("Page: {}".format(str(i+2)))
+                print("Page: {}".format(str(i + 2)))
 
             # we need to be able to clear search and go on to the next role
 
             # go to the clear advanced search page to insert new job description
             driver.get('https://www.indeed.co.uk/advanced_search')
 
-            for link in links:
-                driver.get(link)
-                jd = driver.find_element_by_xpath('//div[@id="jobDescriptionText"]').text
-                descriptions.append(jd)
+        for link in links:
+            driver.get(link)
+            jd = driver.find_element_by_xpath('//div[@id="jobDescriptionText"]').text
+            descriptions.append(jd)
 
-        print(len(Streams), "dstreammming")
         df_stream_description = pd.DataFrame()
         df_stream_description['Title'] = job_titles
         # df_stream_description['Link']=links
         df_stream_description['Description'] = descriptions
 
-        df_stream_description['Stream'] = Streams
+        df_stream_description['Stream'] = streams
 
         return df_stream_description
 
 
 map_of_streams_test = {
     'Business Analysis': ['business analyst'], 'Business Intelligence': ['business intelligence', 'data analyst']}
-
-print(scrape_web_job_description(map_of_streams_test))
+df = scrape_web_job_description(map_of_streams_test)
+print(df.head())
+print(df['Stream'].unique())
